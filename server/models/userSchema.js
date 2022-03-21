9f1d0e159e9816acff14c97e71aa69fef72fa857
nikhilkuruba <nikhil.kuruba237@gmail.com>
2022-03-21 12:39:08 +0530

    model

---
+++ server/models/userSchema.js
@@ -0,0 +1,18 @@
+const mongoose=require('mongoose');
+const {Schema}=mongoose;
+
+const userSchema=new Schema({
+    name:{type:String,required:true},
+    email:{type:String,required:true,unique:true},  
+    phone:{type:Number,required:true},
+    state:{type:String,required:true},
+    district:{type:String,required:true},
+    address:{type:String,required:true},
+    pincode:{type:Number,required:true},  
+    passowrd:{type:String,required:true},
+},    
+    {timestamps:true}
+    );
+    
+const User=mongoose.model('Users',userSchema);
+module.exports=User;
