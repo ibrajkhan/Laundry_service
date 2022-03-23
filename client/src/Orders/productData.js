9c652905f35aa14447dcefaf1e3c5a2fea403bb9
polipalliDevi <deviramakrishnaa8101@gmail.com>
2022-03-23 12:11:28 +0530

    OrdersCreating

---
+++ client/src/Orders/productData.js
@@ -0,0 +1,57 @@
+import React from "react";
+import './main.css'
+
+
+import wash from "../images/wash.png";
+import washClick from "../images/washclick.png";
+import pack from "../images/pack.png";
+import packClick from "../images/packclick.png";
+import iron from "../images/iron.png";
+import ironClick from "../images/ironclick.png";
+import fold from "../images/fold.png";
+import foldclick from "../images/foldclick.png";
+
+function ProductData(props){
+    return (
+    <tr>
+        <td>
+            <div className="row-product">
+                <div className="column">
+                    <img src={props.img} alt="shirt" className="productImg"></img>
+                </div>
+                <div className="column">
+                    <b>{props.title}</b>
+                    <p>{props.descp}</p>
+                </div>
+            </div>
+        </td>
+        <td>
+            <input type="text" maxLength="4" size="1" ></input>
+        </td>
+        <td>
+            <div className="row-washtype">
+                <div className="wash-type">
+                    <img src={wash} alt="wash"></img>
+                </div>
+                <div className="wash-type">
+                    <img src={iron} alt="iron"></img>
+                </div>
+                <div className="wash-type">
+                    <img src={fold} alt="fold"></img>
+                </div>
+                <div className="wash-type">
+                    <img src={pack} alt="pack"></img>
+                </div>
+            </div>
+        </td>
+        <td>
+            <p>5 X 10 = 50</p>
+        </td>
+        <td>
+            <button>Reset</button>
+        </td>
+    </tr>
+    )
+}
+
+export default ProductData;
---
+++ client/src/Orders/productListCreating.js
@@ -0,0 +1,80 @@
+import React from "react";
+import './main.css'
+import ProductData from "./productData";
+import shirts from "../images/shirts.jpg"
+import tshirts from "../images/tshirts.jpg"
+import jeans from "../images/jeans.jpg"
+import boxers from "../images/boxers.jpg"
+import trouses from "../images/trouses.jpg"
+import joggers from "../images/joggers.jpg"
+import others from "../images/others.jpg"
+
+
+
+function  ProductListing(){
+    const productsData = [
+        {
+          img: shirts,
+          title: "Shirt",
+          descp: "Shirts for Laundry",
+        },
+        {
+          img: tshirts,
+          title: "tshirt",
+          descp: "tshirts for laundry ",
+        },
+        {
+          img: jeans,
+          title: "jeans",
+          descp: "jeans for laundry",
+        },
+        {
+          img: boxers,
+          title: "boxers",
+          descp: "boxers for laundry",
+        },
+        {
+          img: trouses,
+          title: "trouses",
+          descp: "trouses for laundry",
+        },
+        {
+          img: joggers,
+          title: "joggers",
+          descp: "joggers for laundry",
+        },
+        {
+          img: others,
+          title: "others",
+          descp: "other items for laundry",
+        },
+      ];
+    
+    return (
+        <div className="table-content">
+            <div></div>
+            <table className="table" cellPadding="10" cellSpacing="30">
+               <thead className="table-head">
+                    <tr className="table-row-darkOne">
+                    <th>Product Types</th>
+                    <th>Qunatity</th>
+                    <th>Wash-Type</th>
+                    <th>Price</th>
+                    <th>Reset</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {productsData.map((product)=>(
+                        <ProductData 
+                        img={product.img}
+                        title={product.title}
+                        descp={product.descp}/>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+
+    )
+}
+
+export default ProductListing;
